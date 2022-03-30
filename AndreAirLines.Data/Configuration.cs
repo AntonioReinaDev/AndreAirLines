44e88ecc2bb2ddc51bac6ca00ca668b00de2bd82
Antonio_Reina <antonioreinadev@hotmail.com>
2022-03-30 20:44:09 -0300

    Add project files.

---
+++ AndreAirLines.Data/Configuration.cs
@@ -0,0 +1,17 @@
+﻿namespace AndreAirLines.Data
+{
+    public static class Configuration
+    {
+        static string datasource = "ANTONIO";
+        static string database = "AirlinesAndre";
+        static string username = "sa";
+        static string password = "afdpnmY#2000";
+        public static string ConnectionString
+        {
+            get
+            {
+                return @$"Data Source={datasource};Initial Catalog={database};Persist Security Info=True;User ID={username};Password={password};";
+            }
+        }
+    }
+}
